4498027198977cdbb9282ba409bb4bd150c7be35
CodeTex <29424456+CodeTex@users.noreply.github.com>
2023-01-14 21:34:11 +0100

    ADD server

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from starlite import Starlite, get
+
+
+@get("/")
+def hello_world() -> dict[str, str]:
+    """Handler function that returns a greeting dictionary."""
+    return {"hello": "world"}
+
+
+app = Starlite(route_handlers=[hello_world])
